de10195f9f894dbdac3edbe845149954ada0f4a0
Alexander Ivanov <alexander.vl.ivanov@gmail.com>
2010-07-12 08:09:13 +0600

    Draft with grandma setup

---
+++ seo/grandma_setup/models.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from grandma.models import BaseSettings
+
+class SeoSettings(BaseSettings):
+    pass
+
+class SeoModel(models.Model):
+    settings = models.ForeignKey(SeoSettings, related_name='models')
+    model = models.CharField(verbose_name=_('Mode'), max_length=255)
