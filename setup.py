9bc475eec6004a0c854632e887a9653c61f11b96
Alexander Ivanov <alexander.vl.ivanov@gmail.com>
2010-07-19 10:27:19 +0600

    Add files

---
+++ setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.  
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="grandma.django-seo",
+    version="0.1.0",
+    description=("Django seo" +
+        " with GrandmaCMS integration"),
+    license="LGPL",
+    keywords="django seo",
+
+    author="Alexander Ivanov",
+    author_email="alexander.ivanov@redsolution.ru",
+
+    maintainer='Alexander Ivanov',
+    maintainer_email='alexander.ivanov@redsolution.ru',
+
+    url="http://packages.python.org/django-seo",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'License :: Freely Distributable',
+        'Natural Language :: Russian',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 2.5',
+        'Topic :: Software Development :: Version Control',
+    ],
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False,
+    long_description=open('README').read(),
+    entry_points={
+        'grandma_setup': ['seo = seo.grandma_setup', ],
+    }
+)
